e388b9c2659802e618b7dc135d0a78576c4ba458
Admin <admin@microservices.agency>
2017-03-08 20:35:09 -0500

    Replaced the bloom filters with a simple cache

--- app.py
+++ app.py
@@ -2,29 +2,16 @@ from flask import Flask, jsonify, request
 from apscheduler.schedulers.background import BackgroundScheduler
 import feedparser
 import maya
-from tinydb import TinyDB
-from tinydb import where
+from tinydb import TinyDB, where, Query
+from tinydb.operations import increment
 import hashlib
 import json
-from pybloom import ScalableBloomFilter
-import pickle
-import codecs
-import sys
 
 scheduler = BackgroundScheduler() # create the scheduler
 
 db = TinyDB('/data/db.json') # initialize the database
 
 app = Flask(__name__) # initialize the Flask app
-
-
-# from http://stackoverflow.com/questions/1094841
-def human_readable_bytes(num):
-    for unit in ['B','KiB','MiB','GiB','TiB','PiB','EiB','ZiB']:
-        if abs(num) < 1024.0:
-            return "%3.1f %s" % (num, unit)
-        num /= 1024.0
-    return "%.1f %s" % (num, 'Yi')
 
 
 def update_feed(feed_id):
@@ -37,31 +24,14 @@ def update_feed(feed_id):
 
         # fetch the feed items, this involves an external HTTP request
         data = feedparser.parse(feed_data['url'])
-        feed_update_time = maya.now().iso8601()
+        feed_fetch_time = maya.now().iso8601()
 
         app.logger.debug("got %s items " % len(data.entries))
 
-        # get the bloom filter for this feed, useful to exclude previously seen items
-        # see https://en.wikipedia.org/wiki/Bloom_filter
-        # and https://blog.medium.com/what-are-bloom-filters-1ec2a50c68ff
-        results = db.table('feeds_bloom_filters').search(where('id') == feed_id)
-        if len(results) == 1:
-            app.logger.debug("loading existing sbf")
-            pickled_sbf = results[0]['bloom_filter']
-
-            # base64 is certainly not the most efficient storage format but I'm not sure TinyDB handles binary data
-            sbf = pickle.loads(codecs.decode(pickled_sbf.encode(), "base64"))
-        else:
-            app.logger.debug("creating new sbf")
-
-            # from the module doc: Scalable Bloom Filters allow your bloom filter bits to grow as a function of
-            # false positive probability and size. When capacity is reached a new filter is then created exponentially
-            # larger than the last with a tighter probability of false positives and a larger number of hash functions.
-            sbf = ScalableBloomFilter(mode=ScalableBloomFilter.SMALL_SET_GROWTH)
-
         # clear the feed_x_items table as we only keep the items from the last fetch
-        db.purge_table("feed_%s_items" % feed_id)
-        feed_items_table = db.table("feed_%s_items" % feed_id)
+        db.purge_table("feed_%s_last_items" % feed_id)
+        feed_items_table = db.table("feed_%s_last_items" % feed_id)
+        feed_cache_table = db.table("feed_%s_cache" % feed_id)
 
         for item in data.entries:
 
@@ -72,54 +42,50 @@ def update_feed(feed_id):
                 'summary' : item.summary
             }
 
+            feed_items_table.insert(new_item) # add it to the last items fetched table
+
             # check if we have seen this item before
-            if item.link not in sbf:
+            results = feed_cache_table.search(where('link') == item.link)
+            if len(results) == 0:
+
                 app.logger.debug("new item => " + item.link)
 
-                sbf.add(item.link)
-
+                # if that is a new item, add it to the cache
+                feed_cache_table.insert({'link': item.link, 'ttl' : 0})
 
                 #
                 # SEND NOTIFICATION OUT
                 #
 
-            feed_items_table.insert(new_item)
+                if 'notifications_sent' not in feed_data:
+                    feed_data['notifications_sent'] = 1
+                else:
+                    feed_data['notifications_sent'] = feed_data['notifications_sent'] + 1
 
-        # update the bloom filter in the db
-        pickled_sbf = codecs.encode(pickle.dumps(sbf), "base64").decode()
-        bloom_filter = {'id': feed_id, 'bloom_filter': pickled_sbf}
 
-        results = db.table('feeds_bloom_filters').search(where('id') == feed_id)
-        if len(results) == 1:
-            app.logger.debug("updating sbf in db")
-            db.table('feeds_bloom_filters').update(bloom_filter, where('id') == feed_id)
-        else:
-            app.logger.debug("inserting sbf in db")
-            db.table('feeds_bloom_filters').insert(bloom_filter)
+        # update the ttl of the items in the cache
+        feed_cache_table.update(increment('ttl'), Query().ttl.exists())
+
+        # remove old entries in the cache
+        item = Query()
+        feed_cache_table.remove(item.ttl > 3)
+
+        feed_data['items_cached'] = len(feed_cache_table)
 
         # update the feed timers
-        if 'time_first_update' not in feed_data:
-            feed_data['time_first_update'] = feed_update_time
+        if 'time_first_fetch' not in feed_data:
+            feed_data['time_first_fetch'] = feed_fetch_time
 
-        feed_data['time_last_update'] = feed_update_time
+        feed_data['time_last_fetch'] = feed_fetch_time
 
         # update the feed items counts
-        if 'items_fetched_total' not in feed_data:
-            feed_data['items_fetched_total'] = len(data.entries)
+        if 'items_fetched' not in feed_data:
+            feed_data['items_fetched'] = len(data.entries)
         else:
-            feed_data['items_fetched_total'] = feed_data['items_fetched_total'] + len(data.entries)
-
-        feed_data['items_unique_count'] = len(sbf)
-
-        # add some debugging info
-        feed_data['sbloom_filter_capacity'] = sbf.capacity
-        feed_data['sbloom_filter_error_rate'] = sbf.error_rate
-        feed_data['sbloom_filter_size_bytes'] = sys.getsizeof(pickled_sbf)
-        feed_data['sbloom_filter_size_human'] = human_readable_bytes(sys.getsizeof(pickled_sbf))
+            feed_data['items_fetched'] = feed_data['items_fetched'] + len(data.entries)
 
         # update the feed data in the db
         db.table('feeds').update(feed_data, where('id') == feed_id)
-
 
 
 @app.before_first_request
@@ -129,7 +95,7 @@ def initialize():
     # recreate any jobs if needed
     feeds = db.table('feeds')
     for feed in feeds.all():
-        scheduler.add_job(update_feed, 'interval', seconds=int(feed['update_interval_secs']), args=[feed['id']], id=feed['id'])
+        scheduler.add_job(update_feed, 'interval', seconds=int(feed['fetch_interval_secs']), args=[feed['id']], id=feed['id'])
 
 
 
@@ -143,14 +109,14 @@ def watch_new_feed():
 
             feed = data['feed']
 
-            if 'url' in feed and 'update_interval_secs' in feed and 'webhook' in feed:
+            if 'url' in feed and 'fetch_interval_secs' in feed and 'webhook' in feed:
 
                 # generate an ID for this new feed
                 m = hashlib.sha256()
                 m.update(str.encode(feed['url']))
                 id = m.hexdigest()[0:6]
 
-                new_feed = {'id': id, 'url': feed['url'], 'update_interval_secs': feed['update_interval_secs'],
+                new_feed = {'id': id, 'url': feed['url'], 'fetch_interval_secs': feed['fetch_interval_secs'],
                             'webhook': feed['webhook']}
 
                 app.logger.debug("creating " + json.dumps(new_feed))
@@ -158,7 +124,7 @@ def watch_new_feed():
                 feeds = db.table('feeds')
                 feeds.insert(new_feed)
 
-                scheduler.add_job(update_feed, 'interval', seconds=int(feed['update_interval_secs']),
+                scheduler.add_job(update_feed, 'interval', seconds=int(feed['fetch_interval_secs']),
                                   args=[new_feed['id']], id=id)
 
 
@@ -174,64 +140,52 @@ def watch_new_feed():
 
 
 @app.route('/feeds')
-def list_watched_feeds():
+def list_all_feeds():
     feeds = db.table('feeds')
     data = {'data': {'feeds': feeds.all()}}
     response = jsonify(data)
     return response
 
 
-
-@app.route('/feeds/<id>')
-def return_feed_items(id):
+@app.route('/feeds/<feed_id>')
+def return_feed_info(feed_id):
 
     feeds = db.table('feeds')
-    results = feeds.search(where('id') == id)
+    results = feeds.search(where('id') == feed_id)
 
     if len(results) == 0:
         msg = {'errors': [{'title': 'Not found', 'details': 'Could not find a feed with the specified ID'}]}
         return jsonify(msg), 404
 
     feed = results[0]
-    feed_items_table = "feed_%s_items" % id
-    feed_items = db.table(feed_items_table)
+    feed_last_items = db.table("feed_%s_last_items" % feed_id)
 
-    data = {'data': {'feed': feed, 'feed_items': feed_items.all()}}
+    data = {'data': {'feed': feed, 'feed_last_items': feed_last_items.all()}}
     response = jsonify(data)
     return response
 
 
-@app.route('/feeds/<id>', methods=['DELETE'])
-def stop_watching_feed(id):
-
-    # unschedule the job related to this feed
-
-    scheduler.remove_job(id)
+@app.route('/feeds/<feed_id>', methods=['DELETE'])
+def stop_watching_feed(feed_id):
 
     # remove the feed from the db
-
     feeds = db.table('feeds')
-    results = feeds.search(where('id') == id)
+    results = feeds.search(where('id') == feed_id)
 
     if len(results) == 0:
         msg = {'errors': [{'title': 'Not found', 'details': 'Could not find a feed with the specified ID'}]}
         return jsonify(msg), 404
+
+    # unschedule the job related to this feed
+    scheduler.remove_job(feed_id)
 
     if len(results) == 1:
         feed = results[0]
         feeds.remove(eids=[feed.eid])
 
     # remove all additional data related to this feed from the db
-
-    db.purge_table("feed_%s_items" % id)
-
-    bfs = db.table('feeds_bloom_filters')
-    results = bfs.search(where('id') == id)
-
-    if len(results) == 1:
-        bf = results[0]
-        bfs.remove(eids=[bf.eid])
-
+    db.purge_table("feed_%s_last_items" % feed_id)
+    db.purge_table("feed_%s_cache" % feed_id)
 
 
 if __name__ == "__main__":
